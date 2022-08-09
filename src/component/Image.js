7f7e46b57d00716bc040f9c5f4eb13cd0fea8d88
theamareshkar <1607152@kiit.ac.in>
2022-08-10 00:59:08 +0530

    pagination added

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 import "./Image.css";
 
+const PER_PAGE = 8;
+
 function Image() {
+  const [currPage, setCurrPage] = useState(0);
   const [item, setItem] = useState([]);
 
   const getPhoto = async () => {
@@ -15,18 +19,37 @@ function Image() {
     getPhoto();
   }, []);
 
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrPage(selectedPage);
+  }
+
+  const offset = currPage * PER_PAGE;
+
+  const currPageData = item.slice(offset, offset + PER_PAGE).map((pic) => {
+    const { thumbnail, id, title } = pic;
+    return (
+      <div className="photo-box">
+        <img src={thumbnail} key={id} className="photo" />
+        <p>{title}</p>
+      </div>
+    );
+  });
+
+  const pageCount = Math.ceil(item.length / PER_PAGE);
   return (
     <div className="container">
       <div className="photos">
-        {item.map((pic) => {
-          const { thumbnail, id, title } = pic;
-          return (
-            <div className="photo-box">
-              <img src={thumbnail} key={id} className="photo" />
-              <p>{title}</p>
-            </div>
-          );
-        })}
+        {currPageData}
+
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination"}
+          previousLinkClassName={"pagination_link"}
+          nextLinkClassName={"pagination_link"}
+        />
       </div>
     </div>
   );
